ee208be1012d115f65a3feaf124015f95ad40116
Camoson343 <ccdecisions@gmail.com>
2020-08-07 08:31:19 -0700

    finish front end aspect of app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import './App.css';
-import {Header} from './components/Header';
+import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Header/>
+    <GlobalProvider>
+      <Header />
       <div className="container">
-        <Balance/>
-        <IncomeExpenses/>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
-    </div>
-  )
+    </GlobalProvider>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-            Hello World! 
+            Expence Tracker - Tutorial by <a href="https://github.com/bradtraversy">Brad Traversy</a><br/>
+            Follow <a href="https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA" >TraversyMedia</a> on YouTube
         </div>
     )
 }
